4a745ff4211bae65862de5cde1ea2c2d88bef8c6
phuongnam2001nam <76032325+phuongnam2001nam@users.noreply.github.com>
2021-02-24 14:37:33 +0700

    a

--- index copy.js
+++ index copy.js
@@ -1,33 +1,44 @@
 
-//
-function gophang() {
-    var a= document.getElementById('slcolumn').value;
-    var b= parseInt(document.getElementById('cellstart').value);
-    var c= parseInt(document.getElementById('cellend').value);
-    var gophang= {"abc":[a, b, c]}
-    var count= '0';
-    var dem= count+ localStorage.getItem('i');
-    localStorage.setItem('i', parseInt(dem)+1);
-    localStorage.setItem(localStorage.getItem('i'), JSON.stringify(gophang));
+function gophang(slcolumn, cellstart, cellend) {
+    if(slcolumn!== undefined) {
+        if(slcolumn=== null) {
+            var a= "thu2";
+            var b= 1;
+            var c= 1;
+        }
+        else {
+            var a= slcolumn;
+            var b= cellstart;
+            var c= cellend;
+        }
+    }
+    if(slcolumn=== undefined) {
+        var a= document.getElementById('slcolumn').value;
+        var b= parseInt(document.getElementById('cellstart').value);
+        var c= parseInt(document.getElementById('cellend').value);
+        var gophang= {"abc":[a, b, c]}
+        var count= '0';
+        var dem= count+ localStorage.getItem('i');
+        localStorage.setItem('i', parseInt(dem)+1);
+        localStorage.setItem(localStorage.getItem('i'), JSON.stringify(gophang));
+    }
+    var b1= b, c1= c;
     if(b< c) {
         var khoangcach= c- b+ 1;
-        for(var i= 0; i< 10; i++) {
-            var gps= document.getElementsByClassName(a)[b-1];
-            gps.setAttribute('rowspan', khoangcach);
-        }
+        var gps= document.getElementsByClassName(a)[b-1];
+        gps.setAttribute('rowspan', khoangcach.toString());
         for(var i= 0; i< khoangcach- 1; i++) {
-            document.getElementsByClassName(a)[b].remove();
+            document.getElementsByClassName(a)[b1].style.display= 'none';
+            b1++;
         }
     }
     if(b> c) {
         var khoangcach= b- c+ 1;
-        for(var i= 0; i< 10; i++) {
-            var gps= document.getElementsByClassName(a)[c-1];
-            gps.setAttribute('rowspan', khoangcach);
-        }
-        
+        var gps= document.getElementsByClassName(a)[c-1];
+        gps.setAttribute('rowspan', khoangcach.toString());
         for(var i= 0; i< khoangcach-1; i++) {
-            document.getElementsByClassName(a)[c].remove();
+            document.getElementsByClassName(a)[c1].style.display= 'none';
+            c1++;
         }
     }
 }
@@ -78,6 +89,7 @@ function TKB() {
             for(var j= 0; j< 10; j++) {
                 var b= j+1;
                 document.getElementsByClassName(thu)[j].firstElementChild.value= TKB.TKB[i][j];
+                console.log(document.getElementsByClassName(thu)[j].firstElementChild.value, TKB.TKB[i][j]);
             }
         }
     }
@@ -86,34 +98,10 @@ function TKB() {
             var x= JSON.parse(localStorage.getItem(i.toString())).abc[0];
             var y= JSON.parse(localStorage.getItem(i.toString())).abc[1];
             var z= JSON.parse(localStorage.getItem(i.toString())).abc[2];
-            gophang1(x, y, z);
+            console.log(x, y, z, localStorage.getItem('i'));
+            gophang(x, y, z);
         }
     }
 }
-function gophang1(slcolumn, cellstart, cellend) {
-    var a= slcolumn;
-    var b= cellstart;
-    var c= cellend;
-    if(b< c) {
-        var khoangcach= c- b+ 1;
-        for(var i= 0; i< 10; i++) {
-            var gps= document.getElementsByClassName(a)[b-1];
-            gps.setAttribute('rowspan', khoangcach);
-        }
-        for(var i= 0; i< khoangcach- 1; i++) {
-            document.getElementsByClassName(a)[b].remove();
-        }
-    }
-    if(b> c) {
-        var khoangcach= b- c+ 1;
-        for(var i= 0; i< 10; i++) {
-            var gps= document.getElementsByClassName(a)[c-1];
-            gps.setAttribute('rowspan', khoangcach);
-        }
-        
-        for(var i= 0; i< khoangcach-1; i++) {
-            document.getElementsByClassName(a)[c].remove();
-        }
-    }
-    
-}
+
+
